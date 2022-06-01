a77a7bdeacdfcc53496fb418cf7286770c4460d7
GBN6 <czesiox8@gmail.com>
2022-06-01 17:08:29 +0200

    Test add function

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const number = document.querySelectorAll('.btn-number');
 const currentDisplay = document.querySelector('.current-number-input');
+const operator = document.querySelectorAll('.btn-oper');
+const typedNumbers = document.querySelector('.saved-number');
 let displayValue = {};
+let firstNumber = null;
+let secondNumber = null;
 console.log(displayValue);
 
 function add(number1, number2)
 {
-    return number1 + number2;
+    return +number1 + +number2;
 }
 
 function subtract(number1, number2)
 {
-    return number1 - number2;
+    return +number1 - +number2;
 }
 
 function multiply(number1, number2)
 {
-    return number1 * number2;
+    return +number1 * +number2;
 }
 
 function divide(number1, number2)
 {
-    return number1 / number2;
+    return +number1 / +number2;
 }
 
 function operate(number1, number2, operator)
@@ -28,19 +32,16 @@ function operate(number1, number2, operator)
     switch(operator) 
     {
         case '+':
-            add(number1, number2)
-            break;
+            return add(number1, number2);
         case '-':
-            subtract(number1, number2)
-            break;
+            return subtract(number1, number2);
         case '*':
-            multiply(number1, number2)
-            break;
+            return multiply(number1, number2);
         case '/':
-            divide(number1, number2)
-            break;            
+            return divide(number1, number2);      
     }  
 }
+
 function displayNumber(e)
 {
     if (currentDisplay.textContent.length < 18)
@@ -48,9 +49,36 @@ function displayNumber(e)
         currentNumber = e.target.textContent;
         currentDisplay.textContent === '0' ? currentDisplay.textContent = currentNumber : currentDisplay.textContent += currentNumber;
         displayValue = currentDisplay.textContent;
-        console.log(displayValue);
         return displayValue;
     }
 }
 
+function calculate(e)
+{
+    currentOperator = e.target.textContent;
+    switch(currentOperator) 
+    {
+        case '+':
+            if (firstNumber === null)
+            {
+                firstNumber = displayValue;
+                currentDisplay.textContent = '0';
+                typedNumbers.textContent += `${firstNumber} + `;
+            }
+            else if (firstNumber !== null)
+            {
+                console.log(`firstNumber ${firstNumber}`)
+                secondNumber = displayValue;
+                console.log(`secondNumber ${secondNumber}`)
+                typedNumbers.textContent += `${secondNumber} + `;
+                currentDisplay.textContent = operate(firstNumber, secondNumber, currentOperator);
+                firstNumber = operate(firstNumber, secondNumber, currentOperator);
+                console.log(`resultNumber ${firstNumber}`)
+            }
+            break;
+    }
+}
+
 number.forEach(element => element.addEventListener('click', displayNumber));
+operator.forEach(element => element.addEventListener('click', calculate));
+console.log(operate(1, 2, '+'));
