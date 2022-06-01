123f0ea1f3a83b5dd4946d1ecf05480198b71579
GBN6 <czesiox8@gmail.com>
2022-06-01 18:05:26 +0200

    Upgrade add function
    Add function can be used fo rest of
    operators to finish of calculator

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let displayValue = {};
 let firstNumber = null;
 let secondNumber = null;
 console.log(displayValue);
+let calculated = false;
 
 function add(number1, number2)
 {
@@ -24,7 +25,8 @@ function multiply(number1, number2)
 
 function divide(number1, number2)
 {
-    return +number1 / +number2;
+    if (number2 === 0) { return "ERROR" }
+    else { return +number1 / +number2; }
 }
 
 function operate(number1, number2, operator)
@@ -44,8 +46,16 @@ function operate(number1, number2, operator)
 
 function displayNumber(e)
 {
+    if (calculated)
+    {
+        currentDisplay.textContent = '';
+        calculated = false;
+    }
     if (currentDisplay.textContent.length < 18)
     {
+        console.log(`firstNumber in display ${firstNumber}`)
+        console.log(`display value ${currentDisplay.textContent}`);
+
         currentNumber = e.target.textContent;
         currentDisplay.textContent === '0' ? currentDisplay.textContent = currentNumber : currentDisplay.textContent += currentNumber;
         displayValue = currentDisplay.textContent;
@@ -73,7 +83,9 @@ function calculate(e)
                 typedNumbers.textContent += `${secondNumber} + `;
                 currentDisplay.textContent = operate(firstNumber, secondNumber, currentOperator);
                 firstNumber = operate(firstNumber, secondNumber, currentOperator);
+                calculated = true;
                 console.log(`resultNumber ${firstNumber}`)
+
             }
             break;
     }
